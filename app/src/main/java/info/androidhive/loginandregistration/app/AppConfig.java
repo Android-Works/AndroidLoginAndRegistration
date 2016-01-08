037e48963e6058b791edd3a39b9f7dab22791285
Chinmay <chinmay235@gmail.com>
2016-01-08 14:23:44 +0530

    Android Login And Registration in PHP

---
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,12 @@
+package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+	// Server user login url
+	//public static String URL_LOGIN = "http://192.168.0.102:8888/android_login_api/login.php";
+	public static String URL_LOGIN = "http://www.raddyx.in/android/login_registration/login.php";
+
+	// Server user register url
+	//public static String URL_REGISTER = "http://192.168.0.102:8888/android_login_api/register.php";
+	public static String URL_REGISTER = "http://www.raddyx.in/android/login_registration/register.php";
+
+}
